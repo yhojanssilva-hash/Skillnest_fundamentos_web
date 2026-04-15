42efa5ed32936e19722baeb4f6cc0cbb772429a5
yhojanssilva-hash <yhojanssilva@liceovvh.cl>
2026-04-15 09:28:50 -0400

    .

--- STACK_CSS/01_configuracion_proyecto_css/static/js/script.js
+++ STACK_CSS/01_configuracion_proyecto_css/static/js/script.js
@@ -2,4 +2,13 @@ console.log("Conexion de js exitosa...");
 
 const nombre = "Yhojans";
 const apellido = "Silva";
+//Unimos ambas constantes con un texto extra 
 console.log("Hola, mi nombre es:" + nombre + " " + apellido);
+
+/*
+2.- Ver el tipo de dato
+*/
+console.log("La variable nombre es un tipo de datos " + typeof nombre);
+
+//Template literals (forma moderna para concatenar) ´´
+ console.log(`Hola, mi nombre es ${nombre} ${apellido}`);
--- STACK_CSS/03_selectores_avanzados_y_color/static/js/script.js
+++ STACK_CSS/03_selectores_avanzados_y_color/static/js/script.js
@@ -2,4 +2,37 @@ console.log("Conexion de js exitosa...");
 
 const nombre = "Yhojans";
 const apellido = "Silva";
+//Unimos ambas constantes con un texto extra 
 console.log("Hola, mi nombre es:" + nombre + " " + apellido);
+
+/*
+2.- Ver el tipo de dato
+*/
+console.log("La variable nombre es un tipo de datos " + typeof nombre);
+
+//Template literals (forma moderna para concatenar) ´´
+ console.log(`Hola, mi nombre es ${nombre} ${apellido}`);
+
+ //Mostrar el largo de un string (texto) - Contar los caracteres.
+ // .length --> Para contar caracteres y espacios.
+ let palabra= "Paralelepipedo";
+ console.log(`La palabra ${palabra} tiene ${palabra.length} letras.`);
+
+ //Crear una frase y contar sus caracteres.
+let frase="Computadora";
+console.log(`La frase ${frase} tiene ${frase.length} caracteres`);
+
+//Metodos comunes en JS para formatear texto
+//Transformar texto en Mayusculas
+let texto1 = "JAvasCriPT Es LO mejor";
+console.log(texto1.toUpperCase());
+
+//Transformar texto a minusculas .toLowerCase
+console.log(texto1-toLowerCase())
+//Buscar un texto dentro de un string
+let texto2 ="Leche, azucar, peras, huevos, harina";
+console.log(texto1.includes("huevos")); //true
+//Convertir una variable a texto
+let telefono = 89182920;
+let telefono_texto = String(telefono);
+console.log(`Mi numero de telefono: ${telefono_texto} es de tipo: ${typeof telefono_texto}`);
