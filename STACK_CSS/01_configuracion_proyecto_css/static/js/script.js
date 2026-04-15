28b880e04411c646a5854591244e9c03fc298afa
yhojanssilva-hash <yhojanssilva@liceovvh.cl>
2026-04-15 08:17:49 -0400

    .

---
+++ STACK_CSS/01_configuracion_proyecto_css/static/js/script.js
@@ -0,0 +1,5 @@
+console.log("Conexion de js exitosa...");
+
+const nombre = "Yhojans";
+const apellido = "Silva";
+console.log("Hola, mi nombre es:" + nombre + " " + apellido);
---
+++ STACK_CSS/02_selectores/static/js/script.js
@@ -0,0 +1,4 @@
+console.log("Conexion de js exitosa...");
+const nombre = "Yhojans";
+const apellido = "Silva";
+console.log("Hola, mi nombre es:" + nombre + " " + apellido);
