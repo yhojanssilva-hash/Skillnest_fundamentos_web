26bbc4888d1a1eab4d6fbdd03cf78f2b9f42083e
yhojanssilva-creator <yhojanssilva@liceovvh.cl>
2026-07-27 09:05:14 -0400

    .

--- STACK_JS/09_objetos_JS/static/js/script.js
+++ STACK_JS/09_objetos_JS/static/js/script.js
@@ -1,3 +1,5 @@
+console.log("Conexion con JS...");
+
 let hamburguesaEspecial = {
     pan: "Pan brioche",
     carne: "Pollo crujiente",
@@ -14,3 +16,31 @@ let hamburguesaEspecial = {
         alert("Extras: " + this.extras.join(", "));
     }
 };
+
+let auto = {
+    marca: "Toyota",
+    modelo: "Corolla",
+    año: 2023,
+    encender: function () {
+        alert("Marca: " + this.marca);
+        alert("Modelo: " + this.modelo);
+        alert("Año: " + this.año);
+        alert("Encendiendo el auto...");
+    }
+};
+
+function mostrarInformacionCasa() {
+    let casa = {
+        direccion: "Av. Siempre Viva 742",
+        habitaciones: 4,
+        baños: 2,
+        MostrarInformacion: function () {
+            alert(`Dirección: ${this.direccion}`);
+            alert(`Habitaciones: ${this.habitaciones}`);
+            alert(`Baños: ${this.baños}`);
+        }
+    };
+
+
+    casa.MostrarInformacion();
+}
