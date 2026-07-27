2f623670fad81d2dd6e0e777340c81fda8a74921
yhojanssilva-creator <yhojanssilva@liceovvh.cl>
2026-07-27 08:27:45 -0400

    .

--- STACK_JS/09_objetos_JS/static/js/script.js
+++ STACK_JS/09_objetos_JS/static/js/script.js
@@ -1,46 +1,16 @@
-console.log ("Conexion js...")
-
-
 let hamburguesaEspecial = {
-
-    pan:"Pan brioche",
-
-    carne:"Pollo crujiente",
-
-    queso:"Suizo",
-
-    extras:[
-
+    pan: "Pan brioche",
+    carne: "Pollo crujiente",
+    queso: "Suizo",
+    extras: [
         "Lechuga",
-
         "Pepinos",
-
         "Miel"
-
     ],
-
-    mostrarIngredientes:function(){
-
-        console.log("Pan:",this.pan);
-
-        console.log("Carne:",this.carne);
-
-        console.log("Queso:",this.queso);
-
-        console.log(
-
-            "Extras:",
-
-            this.extras.join(", ")
-
-        );
-
+    mostrarIngredientes: function () {
+        alert("Pan: " + this.pan);
+        alert("Carne: " + this.carne);
+        alert("Queso: " + this.queso);
+        alert("Extras: " + this.extras.join(", "));
     }
-
 };
-
-
-//Acceder al pan
-
-console.log(hamburguesaEspecial.pan); 
-hamburguesaEspecial.mostrarIngredientes();
