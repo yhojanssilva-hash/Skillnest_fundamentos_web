705b5c218de1064c0e277c210fd697459b2f255b
yhojans silva <yhojanssilva@liceovvh.cl>
2026-07-31 10:32:58 -0400

    .

--- STACK_JS/11_Funciones_en_js/static/js/script.js
+++ STACK_JS/11_Funciones_en_js/static/js/script.js
@@ -43,5 +43,28 @@ Devolver el valor final y mostrar con un alert.
 
 
 function calcularOperacion(a, b, c) {
-return a + b - c;
+    return a + b - c;
+
 }
+let num1 = parseInt(prompt("Ingrese el primer numero:"));
+let num2 = parseInt(prompt("Ingrese el segundo numero:"));
+let num3 = parseInt(prompt("Ingrese el tercer numero:"));
+let resultado = operaciones(num1, num2, num3);
+alert(`La operacion de ${num1} + ${num2} - ${num3} es: ${resultado}`);
+
+
+/*
+Crear una funcion que reciba 1 parametro y permita a traves de un cuble contar hasta este.
+Ej: Se recibe el numero 5 y muestra: 1 - 2 - 3 - 4 - 5
+*/
+let parametro = parseInt(prompt(`Ingrese el limite del contador`));
+function contadorNumeros(a) {
+    let numeros = []
+    for (let i = 1; i <= a; i++) {
+        numeros.push(i);
+
+    }
+    alert(numeros.join(" -  "))
+
+}
+contadorNumeros(parametro)
